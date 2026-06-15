9f8a22c7f5d5e62e4f00cb73fc35f3fbdb395880
arduz-in-zugzwang <sofiarduzcan@gmail.com>
2026-06-15 00:23:45 -0400

    perfil

--- app/src/main/java/com/example/proyecto_lask/ProfileFragment.kt
+++ app/src/main/java/com/example/proyecto_lask/ProfileFragment.kt
@@ -16,6 +16,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ProfileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
+
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
