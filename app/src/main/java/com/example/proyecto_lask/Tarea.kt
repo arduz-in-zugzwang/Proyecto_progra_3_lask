3dcfd4331e1f6aea377466afea75b22c23ea1fd4
luci-solar34 <lucianahola1234o@gmail.com>
2026-06-20 22:35:37 -0400

    Agregr tabla en activity

--- app/src/main/java/com/example/proyecto_lask/Tarea.kt
+++ app/src/main/java/com/example/proyecto_lask/Tarea.kt
@@ -1,10 +1,17 @@
 package com.example.proyecto_lask
 
 import android.os.Bundle
+import android.widget.TableLayout
+import android.widget.TableRow
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Tarea : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +22,69 @@ class Tarea : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        getTags()
+    }
+    private fun getTags() {
+
+        val tableTags =
+            findViewById<TableLayout>(R.id.tableTags)
+
+        CoroutineScope(Dispatchers.IO).launch {
+
+            try {
+
+                val call =
+                    RetrofitClient.create().getTags()
+
+                val response =
+                    call.body()
+
+                withContext(Dispatchers.Main) {
+
+                    if(call.isSuccessful){
+
+                        val data =
+                            response?.data ?: emptyList()
+
+                        data.forEach { tag ->
+
+                            val row =
+                                TableRow(this@Tarea)
+
+                            val tvId =
+                                TextView(this@Tarea)
+
+                            tvId.text =
+                                tag.id.toString()
+
+                            val tvNombre =
+                                TextView(this@Tarea)
+
+                            tvNombre.text =
+                                tag.nombre_tag
+
+                            val tvDescripcion =
+                                TextView(this@Tarea)
+
+                            tvDescripcion.text =
+                                tag.descripcion_tag.toString()
+
+                            row.addView(tvId)
+                            row.addView(tvNombre)
+                            row.addView(tvDescripcion)
+
+                            tableTags.addView(row)
+                        }
+                    }
+                }
+
+            } catch(e: Exception){
+
+                e.printStackTrace()
+
+            }
         }
     }
 }
