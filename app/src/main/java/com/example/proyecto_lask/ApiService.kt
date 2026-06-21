6dcdee4ad0e48b408970b9069de24c5b2a809725
kat <katrinaleigue.24@gmail.com>
2026-06-20 23:21:55 -0400

    corregi lo de descripcion

--- app/src/main/java/com/example/proyecto_lask/ApiService.kt
+++ app/src/main/java/com/example/proyecto_lask/ApiService.kt
@@ -31,7 +31,7 @@ object RetrofitClient{
     fun create(): com.example.proyecto_lask.ApiService{
         val retrofit= Retrofit.Builder()
             //AQUI CAMBIAR EL IP ASAP
-            .baseUrl("http://10.134.40.49/lask_bd/public/")
+            .baseUrl("http://192.168.0.10/lask_bd/public/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
--- app/src/main/java/com/example/proyecto_lask/model/respuestaCreateTag.kt
+++ app/src/main/java/com/example/proyecto_lask/model/respuestaCreateTag.kt
@@ -2,6 +2,7 @@ package com.example.proyecto_lask.model
 
 data class respuestaCreateTag(
     val created_at: String,
+    val descripcion_tag: Any,
     val id: Int,
     val nombre_tag: String,
     val updated_at: String
