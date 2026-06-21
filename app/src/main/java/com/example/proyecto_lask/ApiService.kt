b1c9f9d35dd792d42434ffb72cef14415ae9ef90
kat <katrinaleigue.24@gmail.com>
2026-06-20 20:00:08 -0400

    le agregue patch

--- app/src/main/java/com/example/proyecto_lask/ApiService.kt
+++ app/src/main/java/com/example/proyecto_lask/ApiService.kt
@@ -1,19 +1,27 @@
 package com.example.proyecto_lask
 
 import com.example.proyecto_lask.model.respuestaCreateTag
+import com.example.proyecto_lask.model.respuestaTagPorId
 import com.example.proyecto_lask.model.respuestaTags
+import com.example.proyecto_lask.model.respuestaUpdateTag
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface ApiService {
     @GET(value="api/tags")
     suspend fun getTags(): Response<respuestaTags>
+    @GET(value="api/tags/{id}")
+    suspend fun getTag(@Path("id") id: Int): Response<respuestaTagPorId>
     @POST("api/tags")
     suspend fun createTags(@Field("nombre_tag")nombre_tag: String): Response<respuestaCreateTag>
+    @PATCH("api/tags/{id}")
+    suspend fun actualizarTag(@Path("id") id: Int, @Field("nombre_tag")nombre_tag: String): Response<respuestaUpdateTag>
 }
 object RetrofitClient{
     fun create(): com.example.proyecto_lask.ApiService{
