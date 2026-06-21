95adf22cb5f4415b130800f9fd62d995f1a058a6
kat <katrinaleigue.24@gmail.com>
2026-06-20 22:04:31 -0400

    Mis cambios en ApiService

--- app/src/main/java/com/example/proyecto_lask/ApiService.kt
+++ app/src/main/java/com/example/proyecto_lask/ApiService.kt
@@ -12,6 +12,7 @@ import retrofit2.http.GET
 import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
+import retrofit2.http.DELETE
 
 interface ApiService {
     @GET(value="api/tags")
