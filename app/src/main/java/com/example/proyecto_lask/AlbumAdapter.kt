8901169ad9d72e81a717205e79c1e6314d663787
luci-solar34 <lucianahola1234o@gmail.com>
2026-06-22 00:57:16 -0400

    Home sin ver mas

--- app/src/main/java/com/example/proyecto_lask/AlbumAdapter.kt
+++ app/src/main/java/com/example/proyecto_lask/AlbumAdapter.kt
@@ -7,27 +7,46 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class AlbumAdapter(private val albumes: List<Album>) :
-    RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
+import com.example.proyecto_lask.albumes.Data
+
+class AlbumAdapter(
+    private val albumes: List<Data>
+) : RecyclerView.Adapter<AlbumAdapter.AlbumViewHolder>() {
 
     class AlbumViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val ivPortada: ImageView = view.findViewById(R.id.ivPortadaAlbum)
-        val tvNombre: TextView = view.findViewById(R.id.tvNombreAlbum)
-        val tvArtista: TextView = view.findViewById(R.id.tvArtistaAlbum)
+
+        val tvNombre: TextView =
+            view.findViewById(R.id.tvNombreAlbum)
+
+        val tvArtista: TextView =
+            view.findViewById(R.id.tvArtistaAlbum)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, position: Int): AlbumViewHolder {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): AlbumViewHolder {
+
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_album, parent, false)
+
         return AlbumViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: AlbumViewHolder, position: Int) {
+    override fun onBindViewHolder(
+        holder: AlbumViewHolder,
+        position: Int
+    ) {
+
         val album = albumes[position]
-        holder.tvNombre.text = album.nombre
-        holder.tvArtista.text = album.artista
-        holder.ivPortada.setImageResource(album.portadaResId)
+
+        holder.tvNombre.text =
+            album.nombre_album
+
+        holder.tvArtista.text =
+            "Artista ID: ${album.id_artista}"
     }
 
-    override fun getItemCount(): Int = albumes.size
+    override fun getItemCount(): Int =
+        albumes.size
 }
--- app/src/main/java/com/example/proyecto_lask/ApiService.kt
+++ app/src/main/java/com/example/proyecto_lask/ApiService.kt
@@ -258,7 +258,7 @@ object RetrofitClient{
     fun create(): com.example.proyecto_lask.ApiService{
         val retrofit= Retrofit.Builder()
             //AQUI CAMBIAR EL IP ASAP
-            .baseUrl("http://192.168.0.109/lask_bd/public/")
+            .baseUrl("http://192.168.1.8/lask_bd/public/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
