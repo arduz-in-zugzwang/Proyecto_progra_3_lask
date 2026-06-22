319554342b5cf85357ece9677248f1dc54b72644
kat <katrinaleigue.24@gmail.com>
2026-06-22 16:36:44 -0400

    manifest, y album y song editados

--- app/src/main/java/com/example/proyecto_lask/ApiService.kt
+++ app/src/main/java/com/example/proyecto_lask/ApiService.kt
@@ -262,7 +262,7 @@ object RetrofitClient{
     fun create(): com.example.proyecto_lask.ApiService{
         val retrofit= Retrofit.Builder()
             //AQUI CAMBIAR EL IP ASAP
-            .baseUrl("http://192.168.1.8/lask_bd/public/")
+            .baseUrl("http://192.168.0.102/lask_bd/public/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
